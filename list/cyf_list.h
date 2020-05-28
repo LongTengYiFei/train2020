679bd06c4331177c904f9a8bc04d8719dfa1ad31
LongTengYiFei <378802033@qq.com>
2020-05-28 22:08:41 +0800

    list: some funcion test

--- list/cyf_list.h
+++ list/cyf_list.h
@@ -38,3 +38,57 @@ void list_add(struct list_head *new, struct list_head *head);
 # define POISON_POINTER_DELTA 0
 #endif
 
+
+/**
+ * list_entry - get the struct for this entry
+ * @ptr:	the &struct list_head pointer.
+ * @type:	the type of the struct this is embedded in.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_entry(ptr, type, member) \
+	container_of(ptr, type, member)
+
+/**
+ * list_first_entry - get the first element from a list
+ * @ptr:	the list head to take the element from.
+ * @type:	the type of the struct this is embedded in.
+ * @member:	the name of the list_struct within the struct.
+ *
+ * Note, that list is expected to be not empty.
+ */
+#define list_first_entry(ptr, type, member) \
+	list_entry((ptr)->next, type, member)
+
+
+/**
+ * list_next_entry - get the next element in list
+ * @pos:	the type * to cursor
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_next_entry(pos, member) \
+	list_entry((pos)->member.next, typeof(*(pos)), member)
+
+/**
+ * list_for_each_entry  -       iterate over list of given type
+ * @pos:        the type * to use as a loop cursor.
+ * @head:       the head for your list.
+ * @member:     the name of the list_struct within the struct.
+ */
+#define list_for_each_entry(pos, head, member)                          \
+        for (pos = list_first_entry(head, typeof(*pos), member);        \
+             &pos->member != (head);                                    \
+             pos = list_next_entry(pos, member))
+
+
+/**
+ * list_for_each_entry_safe - iterate over list of given type safe against removal of list entry
+ * @pos:	the type * to use as a loop cursor.
+ * @n:		another type * to use as temporary storage
+ * @head:	the head for your list.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_for_each_entry_safe(pos, n, head, member)			\
+	for (pos = list_first_entry(head, typeof(*pos), member),	\
+		n = list_next_entry(pos, member);			\
+	     &pos->member != (head); 					\
+	     pos = n, n = list_next_entry(n, member))
--- list/mainFrame.c
+++ list/mainFrame.c
@@ -32,6 +32,7 @@ int main(){
 	struct A* pA = malloc(sizeof(struct A));	
 	struct B* pB = malloc(sizeof(struct B));
 	struct C* pC = malloc(sizeof(struct C));
+	struct A* pA2 = malloc(sizeof(struct A));	
 	pA->key = 1;
 	pA->value = 2;
 
@@ -40,12 +41,35 @@ int main(){
 	pB->c = '2';
 
 	pC->ID = 123;
+	
+	pA2->key = 2;
+	pA2->value = 4;
 	//head add
 	//tou cha fa	
 	list_add(&pA->list, &A_list);
 	list_add(&pB->list, &A_list);
 	list_add(&pC->list, &A_list);
-	
+	list_add(&pA2->list, &A_list);
+
+	list_for_each_entry(pA, &A_list, list)
+	{
+    		printf("value = %d\n",pA->value);
+	}
+
+	//remove all
+	struct A *next;
+	list_for_each_entry_safe(pA, next, &A_list, list) 
+	{
+    		free(pA);
+	} 
+
+	//will segment fault
+	list_for_each_entry(pA, &A_list, list)
+	{
+    		printf("value = %d\n",pA->value);
+	}
+
+
 	printf("offsetof(A,key) = %ld\n",offsetof(struct A,key));
 	printf("offsetof(A,value) = %ld\n",offsetof(struct A,value));
 	return 0;
