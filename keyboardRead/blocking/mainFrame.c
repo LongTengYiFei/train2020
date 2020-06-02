007d4936e3a9ad4d4995f53b8a713e9379636b4a
LongTengYiFei <378802033@qq.com>
2020-06-02 05:58:55 +0000

    block over

--- keyboardRead/blocking/mainFrame.c
+++ keyboardRead/blocking/mainFrame.c
@@ -4,23 +4,44 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include<unistd.h>
-#define CHECK_POINT
-int main(){
-	int fd = open("/dev/input/event2",O_RDONLY | O_NONBLOCK);
-	#ifdef CHECK_POINT
-	printf("fd = %d\n",fd);
-	#endif
-	
+
+#define DEV_PATH "/dev/input/event2"
+char keys1[10] = {'q','w','e','r','t','y','u','i','o','p'};//16 -25
+char keys2[9] = {'a','s','d','f','g','h','j','k','l'};//30 - 38
+char keys3[7] = {'z','x','c','v','b','n','m'};//44 - 50
+int main()
+{
+	int fd;
 	struct input_event t;
-	printf("size of t = %d\n",sizeof(t));
-	while(1){	
-		int len = read(fd, &t, sizeof(t));
-		#ifdef CHECK_POINT
-		printf("len = %d\n",len);
-		#endif
+	fd=open(DEV_PATH, O_RDONLY);
+	if(fd <= 0)
+	{
+		printf("open device error!\n");
+		return -1;
 	}
-	
+
+	while(1)
+	{
+		if(read(fd, &t, sizeof(t)) == sizeof(t))
+		{
+			if(t.type==EV_KEY)
+				if(t.value==0 || t.value==1)
+				{	
+					if(t.code <= 25 && t.code >=16)
+						printf("key %c %s\n", keys1[t.code - 16], (t.value) ? "down" : "up");
+					if(t.code <= 38 && t.code >=30)
+						printf("key %c %s\n", keys2[t.code - 30], (t.value) ? "down" : "up");
+					if(t.code <= 50 && t.code >=44)
+						printf("key %c %s\n", keys3[t.code - 44], (t.value) ? "down" : "up");
+					if(t.code == KEY_ESC)
+						break;
+				}
+
+		}
+
+	}
 	close(fd);
 	return 0;
 }
+
+
